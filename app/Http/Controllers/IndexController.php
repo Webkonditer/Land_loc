5023c1efadccf013330924027c998041e962140e
webkonditer <webkonditer@yandex.ru>
2018-09-20 20:13:40 +0300

    Сделаны модели

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\People;
+use App\Page;
+use App\Service;
+use App\Portfolio;
 
 class IndexController extends Controller
 {
     //
+    public function execute(Request $request) {
+
+      return view('layouts/site');
+    }
 }
