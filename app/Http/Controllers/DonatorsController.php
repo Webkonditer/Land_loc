fb72eb28d8894af2c1c6e2e26b96a4444c224422
webkonditer <webkonditer@yandex.ru>
2018-10-05 21:19:25 +0300

    Убраны неподтвержденные платежи и жертвователи

--- app/Http/Controllers/DonatorsController.php
+++ app/Http/Controllers/DonatorsController.php
@@ -19,28 +19,28 @@ class DonatorsController extends Controller
     public function execute(Donator $donators) {
 
       return view('admin.donators.index', [
-        'donators' => Donator::orderBy('created_at', 'desc')->paginate(10)
+        'donators' => Donator::where('last_payment','!=',NULL)->orderBy('created_at', 'desc')->paginate(10)
       ]);
     }
 
     public function execute_sort(Donator $donators, $sort) {
 
       return view('admin.donators.index', [
-        'donators' => Donator::orderBy($sort)->paginate(10)
+        'donators' => Donator::where('last_payment','!=',NULL)->orderBy($sort)->paginate(10)
       ]);
     }
 
     public function execute_monthly(Donator $donators, $sort) {
 
       return view('admin.donators.monthly', [
-        'donators' => Donator::where('monthly', 'Ежемесячно')->orderBy($sort)->paginate(10)
+        'donators' => Donator::where('last_payment','!=',NULL)->where('monthly', 'Ежемесячно')->orderBy($sort)->paginate(10)
       ]);
     }
 
     public function execute_one_time(Donator $donators, $sort) {
 
       return view('admin.donators.one_time', [
-        'donators' => Donator::where('monthly', 'Разово')->orderBy($sort)->paginate(10)
+        'donators' => Donator::where('last_payment','!=',NULL)->where('monthly', 'Разово')->orderBy($sort)->paginate(10)
       ]);
     }
 
@@ -49,6 +49,6 @@ class DonatorsController extends Controller
         $delPayments = Payment::where('donator_id', $donator->id)->delete();
         $donator->delete();
         return redirect()->route('admin.donators');
-        
+
     }
 }
