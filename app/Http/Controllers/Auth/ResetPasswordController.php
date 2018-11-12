da80d5be3e7e753ea980ce7a34395b35c814154f
webkonditer <webkonditer@yandex.ru>
2018-11-12 19:22:01 +0300

    33

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Auth;
 
 class ResetPasswordController extends Controller
 {
@@ -38,7 +39,7 @@ class ResetPasswordController extends Controller
     }
 
     protected function guard()
-    { 
+    {
         return Auth::guard('user_guard');
     }
 }
