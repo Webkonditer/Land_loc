e4a0e368a51873cd4320b793ee287ff752ab8a03
webkonditer <webkonditer@yandex.ru>
2018-12-12 18:05:12 +0300

    Сделана проверка 3 устройств

--- app/Http/Controllers/vega/VideosController.php
+++ app/Http/Controllers/vega/VideosController.php
@@ -9,53 +9,70 @@ use App\VegaPayment;
 use App\VegaUser;
 use App\Format;
 use App\Setting;
+use App\Device;
 use Carbon\Carbon;
 use Cookie;
-use Illuminate\Cookie\CookieJar;
 
 class VideosController extends Controller
 {
     //
     public function __construct() {
-
+      //Проверка авторизации
         $this->middleware('IsUser');
     }
 
-    public function execute(CookieJar $cookieJar, $course, Request $request, VegaUser $vegauser, VegaPayment $vegapayment) {
+    public function execute(Device $device, $course, Request $request, VegaUser $vegauser, VegaPayment $vegapayment) {
 
       $id = Auth::guard('user_guard')->user()->id;
 
       $payment = VegaPayment::where('user_id', $id)->where('course_id', $course)->orderBy('created_at', 'desc')->first();
 
+      //Оплачен ли даный курс
       if (!isset($payment->id)) {
         return redirect()->back()
           ->withErrors('К сожалению мы не нашли в базе сведений об оплате Вами данного курса. Обратитесь пожалуйста в техподдержку.')
           ->withInput();
       }
 
+      //Не исткло ли 90 дней
       $date_of_pay =  Carbon::createFromFormat('Y-m-d H:i:s', $payment->created_at);
       $finish_date = $date_of_pay->addDays(90);
       $current_date = Carbon::now();
-
       if (($current_date->gt($finish_date))) {
         return redirect()->back()
-          ->withErrors('К сожалению время действия Вашего пароля для данного курса уже истекло.')
+          ->withErrors('К сожалению время действия Вашего пароля для данного курса (90 дней) уже истекло.')
           ->withInput();
       }
 
+      //Проверка трех девайсов
+      if (!isset($_COOKIE['dev'])) {
+          $devices = Device::where('user_id', $id)->where('course_id', $course)->get();
+          if ($devices->count() == 3) {
+            return redirect()->back()
+              ->withErrors('К сожалению Вы можете использовать Ваш пароль только на трех устройствах. Обратитесь пожалуйста в техподдержку.')
+              ->withInput();
+          }
+          else {
+            $device->user_id = $id;
+            $device->course_id = $course;
+            $device->device = str_random(8);;
+            $device->save();
+            setcookie("dev", $device->device, time()+7862400);
+          }
+      }
+      else {
+        $search_device = Device::where('user_id', $id)->where('course_id', $course)->where('device', $_COOKIE['dev'])->first();
+        if (!isset($search_device->id)) {
+          return redirect()->back()
+            ->withErrors('!!К сожалению Вы можете использовать Ваш пароль только на трех устройствах. Обратитесь пожалуйста в техподдержку.')
+            ->withInput();
+        }
+      }
 
-//if (!$request->cookie('device')) {
-    Cookie::queue(Cookie::make('name', 'value', 4500));
-    $cookieJar->queue(cookie('name', $request->referrer, 45000));
-//}
 
-// вывод Cookie
-dump($request->cookie());
-//return response('Hello World')->withCookie(cookie('name', 'name',10));
 
-$response = new \Illuminate\Http\Response(view('auth.login'));
-$response->withCookie(cookie('name', $request->referrer, 45000));
-return $response;
+if (isset($_COOKIE['dev'])) dump($_COOKIE['dev']);
+return response('Поздравляем! Вы вошли на страницу курса! Скоро здесь будет долгожданное видео');
 
 
 
